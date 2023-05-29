f114c5a0c905d39261b13a1c36b74718bfc6e4c4
braheezy <michael.c.braha@gmail.com>
2023-05-28 17:11:57 -0700

    Implement favicon support

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ API_URL=$(aws apigatewayv2 get-apis | jq -r '.Items[0].ApiEndpoint')
 sed -i "s/{URL}/${API_URL//\//\\/}/g" visitor_counter.js
 ./resumerk -jsFile visitor_counter.js -cssFile resume.css resume.md
 
+# Insert the favicon link after the closing head tag
+sed -i "s|</head>|</head>\n<link rel=\"icon\" href=\"favicon.ico\">|g" resume.html
+
 echo -e "${GREEN}Successfully converted resume.md:${END}"
 echo -e "\t${BLUE}✅ $(ls resume.pdf)${END}"
 echo -e "\t${BLUE}✅ $(ls resume.html)${END}\n"
